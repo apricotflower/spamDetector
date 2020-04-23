2161a095f616baab9cd45eba3133184df2ffecda
apricotflower <liangliang44@hotmail.com>
2020-04-23 15:37:45 -0400

    trivial change

--- Detector.py
+++ Detector.py
@@ -17,6 +17,7 @@ def start_detector():
     output_file = open(PARAMETER.RESULT, "a+", encoding='utf8')
     p_ham = 1000/1997
     p_spam = 997/1997
+    error_counter = 0
     i = 1
     for (key, value) in all_test_document.items():
         score_ham = math.log(p_ham,10)
@@ -25,7 +26,7 @@ def start_detector():
             if term and term in model_lines:
                 score_ham = score_ham + math.log(float(model_lines[term][3]),10)
                 score_spam = score_spam + math.log(float(model_lines[term][5]),10)
-        if score_ham >= score_spam:
+        if score_ham > score_spam:
             classify_class = PARAMETER.CLASS_HAM
         else:
             classify_class = PARAMETER.CLASS_SPAM
@@ -33,8 +34,10 @@ def start_detector():
             label = PARAMETER.RIGHT
         else:
             label = PARAMETER.WRONG
+            error_counter = error_counter + 1
         output_file.write(str(i) + "  " + PARAMETER.PACKET_TEST + "-" + key + ".txt" + "  " + classify_class + "  " + str(score_ham) + "  " + str(score_spam) + "  " + key.split("-")[0] + "  " + label + "\n")
         i = i+1
+    print("error: " + str(error_counter))
 
 
 def run():
